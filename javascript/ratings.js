a32263070ba2e8d3aae8a851e6da9f6a33d27952
SerjSX <songlife2017@gmail.com>
2021-10-12 15:50:28 +0300

    Changed to JS for rating
    Makes it easier to change ratings

---
+++ javascript/ratings.js
@@ -0,0 +1,65 @@
+const LineageOS = 10;
+const PixelExperience = 9;
+const ParanoidAndroid = "NA";
+const EvolutionX = 8;
+const ArrowOS = 9;
+const DotOS = 10;
+const HentaiOS = "NA";
+const AOSP = "NA";
+const AOKP = "NA";
+const BlissROMS = "NA";
+const SlimROMS = "NA";
+const CarbonROM = "NA";
+
+const TWRP = 10;
+const Skyhawk = 10;
+const Orangefox = 9.5;
+const Pterodon = 1;
+
+const LineageOSRating = document.getElementById("lineageos_rating");
+LineageOSRating.textContent = LineageOS;
+
+const PixelExperienceRating = document.getElementById("pixelexperience_rating");
+PixelExperienceRating.textContent = PixelExperience;
+
+const ParanoidAndroidRating = document.getElementById("paranoidandroid_rating");
+ParanoidAndroidRating.textContent = ParanoidAndroid;
+
+const EvolutionXRating = document.getElementById("evolutionx_rating");
+EvolutionXRating.textContent = EvolutionX;
+
+const ArrowOSRating = document.getElementById("arrowos_rating");
+ArrowOSRating.textContent = ArrowOS;
+
+const DotOSRating = document.getElementById("dotos_rating");
+DotOSRating.textContent = DotOS;
+
+const HentaiOSRating = document.getElementById("hentaios_rating");
+HentaiOSRating.textContent = HentaiOS;
+
+const AOSPRating = document.getElementById("aosp_rating");
+AOSPRating.textContent = AOSP;
+
+const AOKPRating = document.getElementById("aokp_rating");
+AOKPRating.textContent = AOKP;
+
+const BlissROMSRating = document.getElementById("blissroms_rating");
+BlissROMSRating.textContent = BlissROMS;
+
+const SlimRomsRating = document.getElementById("slimroms_rating");
+SlimRomsRating.textContent = SlimROMS;
+
+const CarbonROMRating = document.getElementById("carbonrom_rating");
+CarbonROMRating.textContent = CarbonROM;
+
+const TWRPRating = document.getElementById("twrp_rating");
+TWRPRating.textContent = TWRP;
+
+const SkyhawkRating = document.getElementById("skyhawk_rating");
+SkyhawkRating.textContent = Skyhawk;
+
+const OrangeFoxRating = document.getElementById("orangefox_rating");
+OrangeFoxRating.textContent = Orangefox;
+
+const PterodonRating = document.getElementById("pterodon_rating");
+PterodonRating.textContent = Pterodon;
