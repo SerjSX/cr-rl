0e36ea9c5aa427d52e2b679f486c21d467c8be04
SerjSX <songlife2017@gmail.com>
2021-10-13 09:52:43 +0300

    Added content

---
+++ javascript/recoveries.js
@@ -0,0 +1,61 @@
+/* Sample:
+const customrecoveryName = "";
+const customrecoveryRating = N;
+const customrecoveryWebsite = "";
+const customrecoveryDevices = "";
+const customrecoverySourceCode = "";
+
+document.getElementById("customrecovery_name").textContent = customrecoveryName;
+document.getElementById("customrecovery_rating").textContent = customrecoveryRating;
+document.getElementById("customrecovery_website").setAttribute("href", customrecoveryWebsite);
+document.getElementById("customrecovery_devices").setAttribute("href", customrecoveryDevices);
+document.getElementById("customrecovery_sourcecode").setAttribute("href", customrecoverySourceCode);
+*/
+
+const twrpName = "TWRP";
+const twrpRating = 10;
+const twrpWebsite = "https://twrp.me/";
+const twrpDevices = "https://twrp.me/Devices/";
+const twrpSourceCode = "https://github.com/TeamWin";
+
+document.getElementById("twrp_name").textContent = twrpName;
+document.getElementById("twrp_rating").textContent = twrpRating;
+document.getElementById("twrp_website").setAttribute("href", twrpWebsite);
+document.getElementById("twrp_devices").setAttribute("href", twrpDevices);
+document.getElementById("twrp_sourcecode").setAttribute("href", twrpSourceCode);
+
+const skyhawkName = "Skyhawk";
+const skyhawkRating = 10;
+const skyhawkWebsite = "https://skyhawkrecovery.github.io/";
+const skyhawkDevices = "https://sourceforge.net/projects/shrp/files/";
+const skyhawkSourceCode = "https://github.com/SHRP";
+
+document.getElementById("skyhawk_name").textContent = skyhawkName;
+document.getElementById("skyhawk_rating").textContent = skyhawkRating;
+document.getElementById("skyhawk_website").setAttribute("href", skyhawkWebsite);
+document.getElementById("skyhawk_devices").setAttribute("href", skyhawkDevices);
+document.getElementById("skyhawk_sourcecode").setAttribute("href", skyhawkSourceCode);
+
+const orangefoxName = "Orangefox";
+const orangefoxRating = 9.5;
+const orangefoxWebsite = "https://orangefox.download/";
+const orangefoxDevices = "https://orangefox.download/";
+const orangefoxSourceCode = "https://gitlab.com/OrangeFox/infrastructure/dsite";
+
+document.getElementById("orangefox_name").textContent = orangefoxName;
+document.getElementById("orangefox_rating").textContent = orangefoxRating;
+document.getElementById("orangefox_website").setAttribute("href", orangefoxWebsite);
+document.getElementById("orangefox_devices").setAttribute("href", orangefoxDevices);
+document.getElementById("orangefox_sourcecode").setAttribute("href", orangefoxSourceCode);
+
+const pterodonName = "Pterodon";
+const pterodonRating = 1;
+const pterodonWebsite = "https://github.com/PterodonRecovery";
+const pterodonDevices = "https://github.com/PterodonRecovery";
+const pterodonSourceCode = "https://github.com/PterodonRecovery";
+
+document.getElementById("pterodon_name").textContent = pterodonName;
+document.getElementById("pterodon_rating").textContent = pterodonRating;
+document.getElementById("pterodon_website").setAttribute("href", pterodonWebsite);
+document.getElementById("pterodon_devices").setAttribute("href", pterodonDevices);
+document.getElementById("pterodon_sourcecode").setAttribute("href", pterodonSourceCode);
---
+++ javascript/roms.js
@@ -0,0 +1,157 @@
+/* Sample:
+const customromName = "";
+const customromRating = N;
+const customromWebsite = "";
+const customromDevices = "";
+const customromSourceCode = "";
+
+document.getElementById("customrom_name").textContent = customromName;
+document.getElementById("customrom_rating").textContent = customromRating;
+document.getElementById("customrom_website").setAttribute("href", customromWebsite);
+document.getElementById("customrom_devices").setAttribute("href", customromDevices);
+document.getElementById("customrom_sourcecode").setAttribute("href", customromSourceCode);
+*/
+
+const lineageosName = "LineageOS";
+const lineageosRating = 10;
+const lineageosWebsite = "https://lineageos.org/";
+const lineageosDevices = "https://wiki.lineageos.org/devices/";
+const lineageosSourceCode = "https://github.com/lineageos";
+
+document.getElementById("lineageos_name").textContent = lineageosName;
+document.getElementById("lineageos_rating").textContent = lineageosRating;
+document.getElementById("lineageos_website").setAttribute("href", lineageosWebsite);
+document.getElementById("lineageos_devices").setAttribute("href", lineageosDevices);
+document.getElementById("lineageos_sourcecode").setAttribute("href", lineageosSourceCode);
+
+const pixelexperienceName = "Pixel Experience";
+const pixelexperienceRating = 9;
+const pixelexperienceWebsite = "https://download.pixelexperience.org/";
+const pixelexperienceDevices = "https://download.pixelexperience.org/devices";
+const pixelexperienceSourceCode = "https://github.com/PixelExperience";
+
+document.getElementById("pixelexperience_name").textContent = pixelexperienceName;
+document.getElementById("pixelexperience_rating").textContent = pixelexperienceRating;
+document.getElementById("pixelexperience_website").setAttribute("href", pixelexperienceWebsite);
+document.getElementById("pixelexperience_devices").setAttribute("href", pixelexperienceDevices);
+document.getElementById("pixelexperience_sourcecode").setAttribute("href", pixelexperienceSourceCode);
+
+const evolutionxName = "Evolution X";
+const evolutionxRating = 8;
+const evolutionxWebsite = "https://evolution-x.org/";
+const evolutionxDevices = "https://evolution-x.org/download";
+const evolutionxSourceCode = "https://github.com/Evolution-X";
+
+document.getElementById("evolutionx_name").textContent = evolutionxName;
+document.getElementById("evolutionx_rating").textContent = evolutionxRating;
+document.getElementById("evolutionx_website").setAttribute("href", evolutionxWebsite);
+document.getElementById("evolutionx_devices").setAttribute("href", evolutionxDevices);
+document.getElementById("evolutionx_sourcecode").setAttribute("href", evolutionxSourceCode);
+
+const arrowosName = "ArrowOS";
+const arrowosRating = 9;
+const arrowosWebsite = "https://arrowos.net/";
+const arrowosDevices = "https://arrowos.net/download";
+const arrowosSourceCode = "https://github.com/arrowos";
+
+document.getElementById("arrowos_name").textContent = arrowosName;
+document.getElementById("arrowos_rating").textContent = arrowosRating;
+document.getElementById("arrowos_website").setAttribute("href", arrowosWebsite);
+document.getElementById("arrowos_devices").setAttribute("href", arrowosDevices);
+document.getElementById("arrowos_sourcecode").setAttribute("href", arrowosSourceCode);
+
+const dotosName = "DotOS";
+const dotosRating = 10;
+const dotosWebsite = "https://www.droidontime.com/";
+const dotosDevices = "https://www.droidontime.com/devices";
+const dotosSourceCode = "https://github.com/dotos";
+
+document.getElementById("dotos_name").textContent = dotosName;
+document.getElementById("dotos_rating").textContent = dotosRating;
+document.getElementById("dotos_website").setAttribute("href", dotosWebsite);
+document.getElementById("dotos_devices").setAttribute("href", dotosDevices);
+document.getElementById("dotos_sourcecode").setAttribute("href", dotosSourceCode);
+
+const paranoidandroidName = "Paranoid Android";
+const paranoidandroidRating = "NA";
+const paranoidandroidWebsite = "https://paranoidandroid.co/";
+const paranoidandroidDevices = "https://paranoidandroid.co/downloads";
+const paranoidandroidSourceCode = "https://github.com/AOSPA";
+
+document.getElementById("paranoidandroid_name").textContent = paranoidandroidName;
+document.getElementById("paranoidandroid_rating").textContent = paranoidandroidRating;
+document.getElementById("paranoidandroid_website").setAttribute("href", paranoidandroidWebsite);
+document.getElementById("paranoidandroid_devices").setAttribute("href", paranoidandroidDevices);
+document.getElementById("paranoidandroid_sourcecode").setAttribute("href", paranoidandroidSourceCode);
+
+const hentaiosName = "HentaiOS";
+const hentaiosRating = "NA";
+const hentaiosWebsite = "https://wiki.newcentrix.helluvaos.com/";
+const hentaiosDevices = "https://github.com/hentaiOS-Devices";
+const hentaiosSourceCode = "https://github.com/hentaiOS";
+
+document.getElementById("hentaios_name").textContent = hentaiosName;
+document.getElementById("hentaios_rating").textContent = hentaiosRating;
+document.getElementById("hentaios_website").setAttribute("href", hentaiosWebsite);
+document.getElementById("hentaios_devices").setAttribute("href", hentaiosDevices);
+document.getElementById("hentaios_sourcecode").setAttribute("href", hentaiosSourceCode);
+
+const aospName = "AOSP";
+const aospRating = "NA";
+const aospWebsite = "https://source.android.com/";
+const aospDevices = "https://forum.xda-developers.com/";
+const aospSourceCode = "https://source.android.com/setup/build/downloading";
+
+document.getElementById("aosp_name").textContent = aospName;
+document.getElementById("aosp_rating").textContent = aospRating;
+document.getElementById("aosp_website").setAttribute("href", aospWebsite);
+document.getElementById("aosp_devices").setAttribute("href", aospDevices);
+document.getElementById("aosp_sourcecode").setAttribute("href", aospSourceCode);
+
+const aokpName = "AOKP";
+const aokpRating = "NA";
+const aokpWebsite = "https://aokp.co/";
+const aokpDevices = "https://aokp.co/devices/";
+const aokpSourceCode = "https://aokp.co/source/";
+
+document.getElementById("aokp_name").textContent = aokpName;
+document.getElementById("aokp_rating").textContent = aokpRating;
+document.getElementById("aokp_website").setAttribute("href", aokpWebsite);
+document.getElementById("aokp_devices").setAttribute("href", aokpDevices);
+document.getElementById("aokp_sourcecode").setAttribute("href", aokpSourceCode);
+
+const blissromsName = "BlissROMS";
+const blissromsRating = "NA";
+const blissromsWebsite = "https://blissroms.org/";
+const blissromsDevices = "https://downloads.blissroms.org/";
+const blissromsSourceCode = "https://github.com/BlissRoms";
+
+document.getElementById("blissroms_name").textContent = blissromsName;
+document.getElementById("blissroms_rating").textContent = blissromsRating;
+document.getElementById("blissroms_website").setAttribute("href", blissromsWebsite);
+document.getElementById("blissroms_devices").setAttribute("href", blissromsDevices);
+document.getElementById("blissroms_sourcecode").setAttribute("href", blissromsSourceCode);
+
+const slimromsName = "SlimROMS";
+const slimromsRating = "NA";
+const slimromsWebsite = "https://slimroms.org/";
+const slimromsDevices = "https://slimroms.org/#/download";
+const slimromsSourceCode = "https://github.com/SlimRoms";
+
+document.getElementById("slimroms_name").textContent = slimromsName;
+document.getElementById("slimroms_rating").textContent = slimromsRating;
+document.getElementById("slimroms_website").setAttribute("href", slimromsWebsite);
+document.getElementById("slimroms_devices").setAttribute("href", slimromsDevices);
+document.getElementById("slimroms_sourcecode").setAttribute("href", slimromsSourceCode);
+
+const carbonromName = "CarbonROM";
+const carbonromRating = "NA";
+const carbonromWebsite = "https://carbonrom.org/";
+const carbonromDevices = "https://get.carbonrom.org/";
+const carbonromSourceCode = "https://github.com/CarbonROM";
+
+document.getElementById("carbonrom_name").textContent = carbonromName;
+document.getElementById("carbonrom_rating").textContent = carbonromRating;
+document.getElementById("carbonrom_website").setAttribute("href", carbonromWebsite);
+document.getElementById("carbonrom_devices").setAttribute("href", carbonromDevices);
+document.getElementById("carbonrom_sourcecode").setAttribute("href", carbonromSourceCode);
