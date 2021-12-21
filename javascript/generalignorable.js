ac0335be44a9569d729a4b8e04e77ed8193a261a
Serj <serjfoss@tutanota.com>
2021-12-21 13:58:28 +0200

    UI change

--- javascript/generalignorable.js
+++ javascript/generalignorable.js
@@ -1,49 +1,6 @@
 const nolink = "nolink.html";
 const NA = "N/A";
 
-const customromButton = document.getElementById("custom-rom-button");
-const customromTable = document.getElementById("custom-rom-table");
-const bottomInfo = document.getElementById("bottom-info-js");
-customromButton.addEventListener("click", customromOpen);
-
-function customromOpen() {
-    customromTable.classList.add("add");
-    customrecoveryTable.classList.remove("show");
-    customrecoveryTable.classList.add("hide");
-    customromButton.classList.add("buttononclick");
-    customrecoveryButton.classList.remove("buttononclick");
-    bottomInfo.classList.add("hide");
-    window.setTimeout(customromOpen2, 100);
-}
-
-function customromOpen2() {
-    customromTable.classList.add("show");
-    customrecoveryTable.classList.remove("add");
-    customrecoveryTable.classList.add("remove");
-    bottomInfo.classList.add("remove");
-}
-
-const customrecoveryButton = document.getElementById("custom-recovery-button");
-const customrecoveryTable = document.getElementById("custom-recovery-table");
-customrecoveryButton.addEventListener("click", customrecoveryOpen);
-
-function customrecoveryOpen() {
-    customrecoveryTable.classList.add("add");
-    customromTable.classList.remove("show");
-    customromTable.classList.add("hide");
-    customrecoveryButton.classList.add("buttononclick");
-    customromButton.classList.remove("buttononclick");
-    bottomInfo.classList.add("hide");
-    window.setTimeout(customrecoveryOpen2, 100);
-}
-
-function customrecoveryOpen2() {
-    customrecoveryTable.classList.add("show");
-    customromTable.classList.remove("add");
-    customromTable.classList.add("remove");
-    bottomInfo.classList.add("remove");
-}
-
 const themebutton = document.getElementById("theme-img-js");
 themebutton.addEventListener("click", changeTheme);
 
@@ -51,7 +8,11 @@ var themeState = "light";
 const pageBackground = document.getElementById("background-js");
 const topMenu = document.getElementById("topmenu-js");
 const welcomeBox = document.getElementById("welcome-js");
-const choiceBox = document.getElementById("choice-js");
+const customROMs = document.getElementById("customroms");
+const customRecoveries = document.getElementById("customrecoveries");
+const backUpButton = document.getElementById("back-up-js");
+const tableChoiceButtonONE = document.getElementById("table-choice-button-one-js");
+const tableChoiceButtonTWO = document.getElementById("table-choice-button-two-js");
 
 function changeTheme() {
     if (themeState === "light") {
@@ -60,15 +21,15 @@ function changeTheme() {
         topMenu.style.color = "#ffffff";
         welcomeBox.style.backgroundColor = "#484848";
         welcomeBox.style.color = "#ffffff";
-        choiceBox.style.backgroundColor = "#484848";
-        choiceBox.style.color = "#ffffff";
         themebutton.classList.add("darkbutton");
         themebutton.setAttribute("src", "media/theme/round_light_mode_white_24dp.png");
-        customromButton.classList.add("darkbuttonlight");
-        customrecoveryButton.classList.add("darkbuttonlight");
-        customromTable.style.color = "#ffffff";
-        customrecoveryTable.style.color = "#ffffff";
-        bottomInfo.style.color = "#e4d7d7";
+        customROMs.style.color = "#ffffff";
+        customRecoveries.style.color = "#ffffff";
+        backUpButton.classList.add("darkbutton");
+        backUpButton.setAttribute("src", "media/arrowup/white-24dp/1x/round_arrow_upward_white_24dp.png");
+        tableChoiceButtonONE.classList.add("darkbutton");
+        tableChoiceButtonTWO.classList.add("darkbutton");
+        document.getElementById("bottom-info-js").style.color = "#ffffff";
         themeState = "dark";
     } else if (themeState === "dark") {
         pageBackground.style.backgroundColor = "#ffffff";
@@ -76,15 +37,15 @@ function changeTheme() {
         topMenu.style.color = "black";
         welcomeBox.style.backgroundColor = "#dad8d8b4";
         welcomeBox.style.color = "black";
-        choiceBox.style.backgroundColor = "#dad8d8b4";
-        choiceBox.style.color = "black";
         themebutton.classList.remove("darkbutton");
         themebutton.setAttribute("src", "media/theme/round_dark_mode_black_24dp.png");
-        customromButton.classList.remove("darkbuttonlight");
-        customrecoveryButton.classList.remove("darkbuttonlight");
-        customromTable.style.color = "black";
-        customrecoveryTable.style.color = "black";
-        bottomInfo.style.color = "black";
+        customROMs.style.color = "#000000";
+        customRecoveries.style.color = "#000000";
+        backUpButton.classList.remove("darkbutton");
+        backUpButton.setAttribute("src", "media/arrowup/black-24dp/1x/round_arrow_upward_black_24dp.png");
+        tableChoiceButtonONE.classList.remove("darkbutton");
+        tableChoiceButtonTWO.classList.remove("darkbutton");
+        document.getElementById("bottom-info-js").style.color = "#000000";
         themeState = "light";
     }
 }
